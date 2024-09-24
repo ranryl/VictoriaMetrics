3a146895677e5ecb5f0c5f4f27b914a9570b07ea
Andrii Chubatiuk <andrew.chubatiuk@gmail.com>
2024-09-24 23:03:04 +0300

    use single sync.map for all output states, moved common logic out of outputs logic

--- lib/streamaggr/total.go
+++ lib/streamaggr/total.go
@@ -2,155 +2,100 @@ package streamaggr
 
 import (
 	"math"
-	"sync"
 
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/bytesutil"
 )
 
-// totalAggrState calculates output=total, total_prometheus, increase and increase_prometheus.
-type totalAggrState struct {
-	m sync.Map
-
-	// Whether to reset the output value on every flushState call.
-	resetTotalOnFlush bool
-
-	// Whether to take into account the first sample in new time series when calculating the output value.
-	keepFirstSample bool
+func totalInitFn(resetTotalOnFlush, keepFirstSample bool) aggrValuesInitFn {
+	return func(values []aggrValue) []aggrValue {
+		shared := &totalAggrValueShared{
+			lastValues: make(map[string]totalLastValue),
+		}
+		for i := range values {
+			values[i] = &totalAggrValue{
+				keepFirstSample:   keepFirstSample,
+				resetTotalOnFlush: resetTotalOnFlush,
+				shared:            shared,
+			}
+		}
+		return values
+	}
 }
 
-type totalStateValue struct {
-	mu             sync.Mutex
-	shared         totalState
-	state          [aggrStateSize]float64
-	deleteDeadline int64
-	deleted        bool
-}
-
-type totalState struct {
-	total      float64
-	lastValues map[string]totalLastValueState
-}
-
-type totalLastValueState struct {
+type totalLastValue struct {
 	value          float64
 	timestamp      int64
 	deleteDeadline int64
 }
 
-func newTotalAggrState(resetTotalOnFlush, keepFirstSample bool) *totalAggrState {
-	return &totalAggrState{
-		resetTotalOnFlush:         resetTotalOnFlush,
-		keepFirstSample:           keepFirstSample,
-	}
+type totalAggrValueShared struct {
+	lastValues map[string]totalLastValue
+	total      float64
 }
 
-func (as *totalAggrState) pushSamples(samples []pushSample, deleteDeadline int64, idx int) {
-	var deleted bool
-	for i := range samples {
-		s := &samples[i]
-		inputKey, outputKey := getInputOutputKey(s.key)
+type totalAggrValue struct {
+	total             float64
+	keepFirstSample   bool
+	resetTotalOnFlush bool
+	shared            *totalAggrValueShared
+}
 
-	again:
-		v, ok := as.m.Load(outputKey)
-		if !ok {
-			// The entry is missing in the map. Try creating it.
-			v = &totalStateValue{
-				shared: totalState{
-					lastValues: make(map[string]totalLastValueState),
-				},
-			}
-			outputKey = bytesutil.InternString(outputKey)
-			vNew, loaded := as.m.LoadOrStore(outputKey, v)
-			if loaded {
-				// Use the entry created by a concurrent goroutine.
-				v = vNew
-			}
+func (av *totalAggrValue) pushSample(ctx *pushSampleCtx) {
+	shared := av.shared
+	inputKey := ctx.inputKey
+	lv, ok := shared.lastValues[inputKey]
+	if ok || av.keepFirstSample {
+		if ctx.sample.timestamp < lv.timestamp {
+			// Skip out of order sample
+			return
 		}
-		sv := v.(*totalStateValue)
-		sv.mu.Lock()
-		deleted = sv.deleted
-		if !deleted {
-			lv, ok := sv.shared.lastValues[inputKey]
-			if ok || as.keepFirstSample {
-				if s.timestamp < lv.timestamp {
-					// Skip out of order sample
-					sv.mu.Unlock()
-					continue
-				}
-
-				if s.value >= lv.value {
-					sv.state[idx] += s.value - lv.value
-				} else {
-					// counter reset
-					sv.state[idx] += s.value
-				}
-			}
-			lv.value = s.value
-			lv.timestamp = s.timestamp
-			lv.deleteDeadline = deleteDeadline
-
-			inputKey = bytesutil.InternString(inputKey)
-			sv.shared.lastValues[inputKey] = lv
-			sv.deleteDeadline = deleteDeadline
-		}
-		sv.mu.Unlock()
-		if deleted {
-			// The entry has been deleted by the concurrent call to flushState
-			// Try obtaining and updating the entry again.
-			goto again
+		if ctx.sample.value >= lv.value {
+			av.total += ctx.sample.value - lv.value
+		} else {
+			// counter reset
+			av.total += ctx.sample.value
 		}
 	}
+	lv.value = ctx.sample.value
+	lv.timestamp = ctx.sample.timestamp
+	lv.deleteDeadline = ctx.deleteDeadline
+
+	inputKey = bytesutil.InternString(inputKey)
+	shared.lastValues[inputKey] = lv
 }
 
-func (as *totalAggrState) getSuffix() string {
-        // Note: this function is at hot path, so it shouldn't allocate.
-        if as.resetTotalOnFlush {
-                if as.keepFirstSample {
-                        return "increase"
-                }
-                return "increase_prometheus"
-        }
-        if as.keepFirstSample {
-                return "total"
-        }
-        return "total_prometheus"
+func (av *totalAggrValue) flush(ctx *flushCtx, key string) {
+	suffix := av.getSuffix()
+	// check for stale entries
+	total := av.shared.total + av.total
+	av.total = 0
+	lvs := av.shared.lastValues
+	for lk, lv := range lvs {
+		if ctx.flushTimestamp > lv.deleteDeadline {
+			delete(lvs, lk)
+		}
+	}
+	if av.resetTotalOnFlush {
+		av.shared.total = 0
+	} else if math.Abs(total) >= (1 << 53) {
+		// It is time to reset the entry, since it starts losing float64 precision
+		av.shared.total = 0
+	} else {
+		av.shared.total = total
+	}
+	ctx.appendSeries(key, suffix, total)
 }
 
-func (as *totalAggrState) flushState(ctx *flushCtx) {
-	var total float64
-	m := &as.m
-	suffix := as.getSuffix()
-	m.Range(func(k, v interface{}) bool {
-		sv := v.(*totalStateValue)
-
-		sv.mu.Lock()
-		// check for stale entries
-		deleted := ctx.flushTimestamp > sv.deleteDeadline
-		if deleted {
-			// Mark the current entry as deleted
-			sv.deleted = deleted
-			sv.mu.Unlock()
-			m.Delete(k)
-			return true
+func (av *totalAggrValue) getSuffix() string {
+	// Note: this function is at hot path, so it shouldn't allocate.
+	if av.resetTotalOnFlush {
+		if av.keepFirstSample {
+			return "increase"
 		}
-		total = sv.shared.total + sv.state[ctx.idx]
-		for k1, v1 := range sv.shared.lastValues {
-			if ctx.flushTimestamp > v1.deleteDeadline {
-				delete(sv.shared.lastValues, k1)
-			}
-		}
-		sv.state[ctx.idx] = 0
-		if !as.resetTotalOnFlush {
-			if math.Abs(total) >= (1 << 53) {
-				// It is time to reset the entry, since it starts losing float64 precision
-				sv.shared.total = 0
-			} else {
-				sv.shared.total = total
-			}
-		}
-		sv.mu.Unlock()
-		key := k.(string)
-		ctx.appendSeries(key, suffix, total)
-		return true
-	})
+		return "increase_prometheus"
+	}
+	if av.keepFirstSample {
+		return "total"
+	}
+	return "total_prometheus"
 }
